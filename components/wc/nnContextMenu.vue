42483e7c6f4424fe54547730d270ee867b2c7dea
zhougonglai <838048635@qq.com>
2020-05-19 20:36:33 +0800

    type: update
    wc:
    - ContextMenu
    - ContextMenus
    - DropDown

---
+++ components/wc/nnContextMenu.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.nn-context-menu(v-bind="$attrs" @click.prevent="menuClick")
+  slot
+</template>
+<script>
+export default {
+  name: 'NnContextMenu',
+  props: {
+    name: {
+      type: [String, Number],
+      default: '',
+    },
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+  methods: {
+    menuClick() {
+      this.$parent.$emit('menuClick', this.name)
+      this.$parent.outClick()
+    },
+  },
+}
+</script>
