88d4c355955c68cacf1948c5cd46fa04cc676dd1
zzk <914066828@qq.com>
2020-05-21 17:15:34 +0800

    社区头部样式

--- components/channel/SideBar.vue
+++ components/channel/SideBar.vue
@@ -29,8 +29,11 @@ export default {
     height: 136px;
     display: grid;
     background-image: url('http://placekitten.com/240/136');
+    background-color: #00000080;
+    background-blend-mode: color-dodge;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(2, 1fr);
+    color: #fff;
     .panel {
       padding: 10px;
       display: flex;
